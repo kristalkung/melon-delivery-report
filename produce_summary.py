845d800fe2dca0f3e05c806ca9707bf48d8d094b
Kristal <kristalkung@gmail.com>
2021-01-20 20:37:36 +0000

    Wrote a function to generalize code, added comments, called function for each day with its corresponding text file

--- produce_summary.py
+++ produce_summary.py
@@ -1,40 +1,27 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def melon_report(day, file):
+    """Using the given day and file, prints the count of melon, the type of melon sold, and the amount made"""
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
+    print(f"Day {day}")
+    # print the day and day number
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+    the_file = open(file)
+    # set the_file to open the the given parameter, file
 
+    for line in the_file:
+        line = line.rstrip()
+        # removes any trailing characters
+        words = line.split('|')
+        # splits each line into a list, separated by the |
 
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+        #prints count, melon type, and amount made
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+    the_file.close()
 
-
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+melon_report("1", "um-deliveries-20140519.txt")
+melon_report("2", "um-deliveries-20140520.txt")
+melon_report("3", "um-deliveries-20140521.txt")
